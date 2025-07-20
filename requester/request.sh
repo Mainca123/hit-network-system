295f32635dcc8bbd1132ae35f33fd268301905c3
Mainca123 <giangchinhtgp@gmail.com>
2025-07-20 09:16:58 +0700

    fix backend only

--- requester/request.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while true
-do
-  echo "Sending request to backend..."
-  curl -X 'GET' 'https://hit-network-system.onrender.com/api/v1/auth/load' -H 'accept: */*'
-  echo -e "\n--- Request sent at $(date) ---"
-  sleep 60*5  # 600 giây = 10 phút
-done
