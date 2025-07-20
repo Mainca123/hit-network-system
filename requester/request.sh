08919896a00745c803294f574f93b7338468fd0c
Mainca123 <giangchinhtgp@gmail.com>
2025-07-20 08:38:02 +0700

    fix time auto load

--- requester/request.sh
+++ requester/request.sh
@@ -5,5 +5,5 @@ do
   echo "Sending request to backend..."
   curl -X 'GET' 'https://hit-network-system.onrender.com/api/v1/auth/load' -H 'accept: */*'
   echo -e "\n--- Request sent at $(date) ---"
-  sleep 5  # 600 giây = 10 phút
+  sleep 60*5  # 600 giây = 10 phút
 done
